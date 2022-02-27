167f5d1050818630d300173303abaaf289e0a0d0
32332 <23edd>
2022-02-27 15:45:26 +0300

    Initial Commit

---
+++ Melody/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Melody
+//
+//  Created by Вадим Лавор on 27.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
